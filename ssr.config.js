1c111b94922891cdaa4068c750637906673d3baf
Tastemeru <go@tasteme.ru>
2020-05-18 19:46:57 +0300

    server side

---
+++ ssr.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  template: './index.html',
+  maxAge: 60 * 60 * 1000,
+  config: {
+    basePath: 'https://docs.pushflow.net/',
+    homepage: 'README.md',
+    auto2top: true,
+    loadNavbar: true,
+    loadSidebar: true,
+    maxLevel: 3,
+    subMaxLevel: 3,
+  },
+};
